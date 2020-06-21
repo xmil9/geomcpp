211626162ec8d5d2391ba4ebf6238cfb851fe30b
Mike Lindner <mlindner9@yahoo.com>
2020-06-21 16:28:07 -0400

    Add dec interval and vector tests to geom tests.

--- tests/geomcpp_tests.cpp
+++ tests/geomcpp_tests.cpp
@@ -4,13 +4,17 @@
 // Jun-2020, Michael Lindner
 // MIT license
 //
+#include "interval_dec_tests.h"
 #include "interval_tec_tests.h"
+#include "vec2_tests.h"
 #include <iostream>
 
 
 int main()
 {
+   testDecInterval();
    testTecInterval();
+   testVector2D();
 
    std::cout << "geomcpp tests finished.\n";
    return EXIT_SUCCESS;
