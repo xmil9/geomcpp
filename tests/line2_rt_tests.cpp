a8806555f47b0a83bee6231b3e6106e0245393f5
Mike Lindner <mlindner9@yahoo.com>
2020-06-30 20:40:05 -0400

    Tests for Line2 isPoint, hasStartPoint, startPoint, hasEndPoint, endPoint.

--- tests/line2_rt_tests.cpp
+++ tests/line2_rt_tests.cpp
@@ -60,6 +60,79 @@ void testLine2ValueCtor()
    }
 }
 
+
+void testLine2IsPoint()
+{
+   {
+      const std::string caseLabel = "Line2::isPoint for line that is not a point";
+
+      const Point2 anchor{1.0f, 2.0f};
+      const Vec2 dir{3.0f, 2.0f};
+      const Line2 l{anchor, dir};
+      VERIFY(!l.isPoint(), caseLabel);
+   }
+   {
+      const std::string caseLabel = "Line2::isPoint for line that is a point";
+
+      const Point2 anchor{1, 3};
+      const Vec2 dir{0, 0};
+      const Line2 l{anchor, dir};
+      VERIFY(l.isPoint(), caseLabel);
+   }
+}
+
+
+void testLine2HasStartPoint()
+{
+   {
+      const std::string caseLabel = "Line2::hasStartPoint";
+
+      const Point2 anchor{1.0f, 2.0f};
+      const Vec2 dir{3.0f, 2.0f};
+      const Line2 l{anchor, dir};
+      VERIFY(!l.hasStartPoint(), caseLabel);
+   }
+}
+
+
+void testLine2StartPoint()
+{
+   {
+      const std::string caseLabel = "Line2::startPoint";
+
+      const Point2 anchor{1.0f, 2.0f};
+      const Vec2 dir{3.0f, 2.0f};
+      const Line2 l{anchor, dir};
+      VERIFY(!l.startPoint(), caseLabel);
+   }
+}
+
+
+void testLine2HasEndPoint()
+{
+   {
+      const std::string caseLabel = "Line2::hasEndPoint";
+
+      const Point2 anchor{1.0f, 2.0f};
+      const Vec2 dir{3.0f, 2.0f};
+      const Line2 l{anchor, dir};
+      VERIFY(!l.hasEndPoint(), caseLabel);
+   }
+}
+
+
+void testLine2EndPoint()
+{
+   {
+      const std::string caseLabel = "Line2::endPoint";
+
+      const Point2 anchor{1.0f, 2.0f};
+      const Vec2 dir{3.0f, 2.0f};
+      const Line2 l{anchor, dir};
+      VERIFY(!l.endPoint(), caseLabel);
+   }
+}
+
 } // namespace
 
 
@@ -67,4 +140,9 @@ void testRtLine2()
 {
    testLine2DefaultCtor();
    testLine2ValueCtor();
+   testLine2IsPoint();
+   testLine2HasStartPoint();
+   testLine2StartPoint();
+   testLine2HasEndPoint();
+   testLine2EndPoint();
 }
