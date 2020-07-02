e56f0af7c2fc8a7d0644c2c8fc76aae1770e4284
Mike Lindner <mlindner9@yahoo.com>
2020-07-02 19:04:28 -0400

    LineSeg2 - Syntax fixes.

--- line_seg2_rt.h
+++ line_seg2_rt.h
@@ -25,17 +25,16 @@ template <typename T> class LineSeg2 : public Line2<T>
  public:
    using value_type = typename Line2<T>::value_type;
    using Fp = typename Line2<T>::Fp;
-   using ParametricValue = typename Line2<T>::ParametricValue;
 
    LineSeg2() = default;
    LineSeg2(const Point2<T>& start, const Point2<T>& end);
    LineSeg2(const Point2<T>& start, const Vec2<T>& direction);
 
    bool hasStartPoint() const override { return true; }
-   std::optional<Point2<T>> startPoint() const override { return anchor(); }
+   std::optional<Point2<T>> startPoint() const override { return this->anchor(); }
    bool hasEndPoint() const override { return true; }
    std::optional<Point2<T>> endPoint() const override;
-   std::optional<ParametricValue> isPointOnLine(const Point2<T>& pt) const override;
+   std::optional<Fp> isPointOnLine(const Point2<T>& pt) const override;
 
    Point2<T> midPoint() const;
    Fp lengthSquared() const;
@@ -45,50 +44,53 @@ template <typename T> class LineSeg2 : public Line2<T>
 
 template <typename T>
 LineSeg2<T>::LineSeg2(const Point2<T>& start, const Point2<T>& end)
-: Line2(start, Vec2(start, end))
+: Line2<T>(start, Vec2<T>(start, end))
 {
 }
 
 
 template <typename T>
 LineSeg2<T>::LineSeg2(const Point2<T>& start, const Vec2<T>& direction)
-: Line2(start, direction)
+: Line2<T>(start, direction)
 {
 }
 
 
 template <typename T> std::optional<Point2<T>> LineSeg2<T>::endPoint() const
 {
-   return anchor() + direction();
+   return this->anchor() + this->direction();
 }
 
 
 template <typename T>
-std::optional<typename LineSeg2<T>::ParametricValue>
+std::optional<typename LineSeg2<T>::Fp>
 LineSeg2<T>::isPointOnLine(const Point2<T>& pt) const
 {
-   const auto paramVal = calcParametricValue(pt);
-   if (paramVal && sutil::fpGreaterEqual(*paramVal, 0) && sutil::fpLessEqual(paramVal, 1))
-      return paramVal;
+   const auto factor = this->calcLerpFactor(pt);
+   if (factor && sutil::fpGreaterEqual<Fp>(*factor, 0) &&
+       sutil::fpLessEqual<Fp>(*factor, 1))
+   {
+      return factor;
+   }
    return std::nullopt;
 }
 
 
 template <typename T> Point2<T> LineSeg2<T>::midPoint() const
 {
-   return startPoint().offset(direction() * typename Line2<T>::Fp(0.5));
+   return *startPoint() + (this->direction() * Fp(0.5));
 }
 
 
 template <typename T> typename LineSeg2<T>::Fp LineSeg2<T>::lengthSquared() const
 {
-   return direction().lengthSquared();
+   return this->direction().lengthSquared();
 }
 
 
 template <typename T> typename LineSeg2<T>::Fp LineSeg2<T>::length() const
 {
-   return direction().length();
+   return this->direction().length();
 }
 
 } // namespace rt
