5edda540a9fbec08187ca0b4157347d891348c13
Mike Lindner <mlindner9@yahoo.com>
2020-07-18 20:47:42 -0400

    Implement isConvexPath() in geometry utilities.

--- geom_util.h
+++ geom_util.h
@@ -6,7 +6,10 @@
 // MIT license
 //
 #pragma once
+#include "geom_types.h"
 #include "rect.h"
+#include "vec2.h"
+#include <iterator>
 #include <optional>
 
 
@@ -19,28 +22,67 @@ template <typename T, typename PointIter>
 std::optional<Rect<T>> calcPathBounds(PointIter first, PointIter last)
 {
    // No points.
-	if (first == last)
+   if (first == last)
       return std::nullopt;
 
    // One point.
    const Point2<T>& prev = *first;
-	if (++first == last)
+   if (++first == last)
       return std::make_optional(Rect<T>(prev, prev));
 
    Rect<T> bounds{prev, *first};
    while (++first != last)
    {
-		if (first->x() < bounds.left())
-			bounds.setLeft(first->x());
-		if (first->y() < bounds.top())
-			bounds.setTop(first->y());
-		if (first->x() > bounds.right())
-			bounds.setRight(first->x());
-		if (first->y() > bounds.bottom())
-			bounds.setBottom(first->y());
+      if (first->x() < bounds.left())
+         bounds.setLeft(first->x());
+      if (first->y() < bounds.top())
+         bounds.setTop(first->y());
+      if (first->x() > bounds.right())
+         bounds.setRight(first->x());
+      if (first->y() > bounds.bottom())
+         bounds.setBottom(first->y());
    }
-		
+
    return std::make_optional(bounds);
+}
+
+
+// Checks if given points form a convex path.
+// Convex path - All edges bend in the same direction and don't cross.
+template <typename T, typename PointIter>
+bool isConvexPath(PointIter first, PointIter last)
+{
+   const std::size_t numPts = std::distance(first, last);
+   if (numPts <= 3)
+      return true;
+
+   std::optional<Vec2<T>> prev;
+   std::optional<Vec2<T>> next;
+   Orientation orient = Orientation::None;
+
+   for (PointIter it = first; it != last; ++it)
+   {
+      PointIter nextIt = (it + 1 == last) ? first : it + 1;
+      prev = next;
+      next = std::make_optional(Vec2<T>(*it, *nextIt));
+      if (!prev)
+         continue;
+
+      auto val = perpDot(prev, next);
+      if (sutil::equal(val, 0))
+         continue;
+
+      if (orient == Orientation::None)
+         orient = sutil::greater(val, 0) ? Orientation::Cw : Orientation::Ccw;
+
+      // Orientation cannot change for convex paths.
+      Orientation edgeOrient =
+         sutil::greater(val, 0) ? Orientation::Cw : Orientation::Ccw;
+      if (edgeOrient != orient)
+         return false;
+   }
+
+   return true;
 }
 
 } // namespace geom
