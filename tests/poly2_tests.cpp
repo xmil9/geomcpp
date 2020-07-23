75a7ed3a4ad2741561696400b92ced101bc6ccd2
Mike Lindner <mlindner9@yahoo.com>
2020-07-23 08:40:12 -0400

    Polygons - Tests for cutConvexPolygon.

--- tests/poly2_tests.cpp
+++ tests/poly2_tests.cpp
@@ -432,7 +432,7 @@ void testPoly2Inequality()
 }
 
 
-void testPoly2IsPointInsideConvexPolygon()
+void testIsPointInsideConvexPolygon()
 {
    {
       const std::string caseLabel =
@@ -498,6 +498,138 @@ void testPoly2IsPointInsideConvexPolygon()
    }
 }
 
+
+void testCutConvexPolygon()
+{
+   {
+      const std::string caseLabel = "cutConvexPolygon for empty polygon";
+
+      ct::LineInf2<double> l(Point2(1.0, 2.0), Vec2(2.0, 1.0));
+      Poly2<double> p;
+
+      const std::vector<Poly2<double>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 1, caseLabel);
+      if (res.size() > 0)
+         VERIFY(res[0].size() == 0, caseLabel);
+   }
+   {
+      const std::string caseLabel = "cutConvexPolygon for polygon with single point";
+
+      ct::LineInf2<int> l(Point2(1, 2), Vec2(2, 1));
+      Poly2<int> p{Point2{1, 1}};
+
+      const std::vector<Poly2<int>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 1, caseLabel);
+      if (res.size() > 0)
+         VERIFY(res[0] == p, caseLabel);
+   }
+   {
+      const std::string caseLabel =
+         "cutConvexPolygon for polygon with two points crossing the line";
+
+      ct::LineInf2<double> l(Point2(1.0, 2.0), Vec2(2.0, 1.0));
+      Poly2<double> p{Point2{1.0, 1.0}, Point2{2.0, 5.0}};
+
+      const std::vector<Poly2<double>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 2, caseLabel);
+      if (res.size() == 2)
+      {
+         VERIFY(res[0].size() == 2, caseLabel);
+         VERIFY(res[1].size() == 2, caseLabel);
+      }
+   }
+   {
+      const std::string caseLabel =
+         "cutConvexPolygon for polygon with two points on the same side of the line";
+
+      ct::LineInf2<float> l(Point2(1.0f, 2.0f), Vec2(2.0f, 1.0f));
+      Poly2<float> p{Point2{3.0f, 4.0f}, Point2{2.0f, 5.0f}};
+
+      const std::vector<Poly2<float>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 1, caseLabel);
+      if (res.size() == 1)
+         VERIFY(res[0] == p, caseLabel);
+   }
+   {
+      const std::string caseLabel =
+         "cutConvexPolygon for polygon with two points on the line";
+
+      ct::LineInf2<double> l(Point2(1.0, 2.0), Vec2(2.0, 1.0));
+      Poly2<double> p{Point2{3.0, 3.0}, Point2{5.0, 4.0}};
+
+      const std::vector<Poly2<double>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 1, caseLabel);
+      if (res.size() == 1)
+         VERIFY(res[0] == p, caseLabel);
+   }
+   {
+      const std::string caseLabel =
+         "cutConvexPolygon for polygon intersected by the line";
+
+      ct::LineInf2<double> l(Point2(1.0, 2.0), Vec2(2.0, 1.0));
+      Poly2<double> p{Point2{1.0, -1.0}, Point2{7.0, 1.0}, Point2{6.0, 2.0},
+                      Point2{4.0, 6.0}, Point2{2.0, 5.0}};
+
+      const std::vector<Poly2<double>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 2, caseLabel);
+      if (res.size() == 2)
+      {
+         VERIFY(res[0].size() == 5, caseLabel);
+         VERIFY(res[1].size() == 4, caseLabel);
+      }
+   }
+   {
+      const std::string caseLabel =
+         "cutConvexPolygon for polygon not intersected by the line";
+
+      ct::LineInf2<double> l(Point2(10.0, 10.0), Vec2(2.0, 1.0));
+      Poly2<double> p{Point2{1.0, -1.0}, Point2{7.0, 1.0}, Point2{6.0, 2.0},
+                      Point2{4.0, 6.0}, Point2{2.0, 5.0}};
+
+      const std::vector<Poly2<double>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 1, caseLabel);
+      if (res.size() == 1)
+         VERIFY(res[0] == p, caseLabel);
+   }
+   {
+      const std::string caseLabel = "cutConvexPolygon for polygon touched by the line";
+
+      ct::LineInf2<double> l(Point2(7.0, 1.0), Vec2(-1.0, -1.0));
+      Poly2<double> p{Point2{1.0, -1.0}, Point2{7.0, 1.0}, Point2{6.0, 2.0},
+                      Point2{4.0, 6.0}, Point2{2.0, 5.0}};
+
+      const std::vector<Poly2<double>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 1, caseLabel);
+      if (res.size() == 1)
+         VERIFY(res[0] == p, caseLabel);
+   }
+   {
+      const std::string caseLabel = "cutConvexPolygon for polygon intersected by the "
+                                    "line with all but one point on the same side";
+
+      ct::LineInf2<double> l(Point2(6.0, -2.0), Vec2(1.0, 4.0));
+      Poly2<double> p{Point2{1.0, -1.0}, Point2{8.0, 1.0}, Point2{6.0, 2.0},
+                      Point2{4.0, 6.0}, Point2{2.0, 5.0}};
+
+      const std::vector<Poly2<double>> res = cutConvexPolygon(p, l);
+
+      VERIFY(res.size() == 2, caseLabel);
+      if (res.size() == 2)
+      {
+         VERIFY(res[0].size() == 3, caseLabel);
+         VERIFY(res[1].size() == 6, caseLabel);
+      }
+   }
+}
+
 } // namespace
 
 
@@ -521,5 +653,6 @@ void testPoly2()
    testPoly2IsConvex();
    testPoly2Equality();
    testPoly2Inequality();
-   testPoly2IsPointInsideConvexPolygon();
+   testIsPointInsideConvexPolygon();
+   testCutConvexPolygon();
 }
