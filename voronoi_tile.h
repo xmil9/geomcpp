07f55f0f79c44e6fa69d17976143efbff53e01fa
Mike Lindner <mlindner9@yahoo.com>
2020-10-07 08:42:29 -0400

    Voronoi tile - Shorten containsVertex to contains and fix implementation.

--- voronoi_tile.h
+++ voronoi_tile.h
@@ -21,14 +21,14 @@ template <typename T> class VoronoiTile
    ~VoronoiTile() = default;
    VoronoiTile(const VoronoiTile&) = default;
    VoronoiTile(VoronoiTile&&) = default;
-   
+
    VoronoiTile& operator=(const VoronoiTile&) = default;
    VoronoiTile& operator=(VoronoiTile&&) = default;
 
    const Point2<T>& seed() const { return m_seed; }
-   const Point2<T>& outline() const { return m_outline; }
-   std::size_t countVertices() const { return m_outline.size(); }
-   bool containsVertex(const Point2<T>& pt) const { return m_outline.contains(pt); }
+   const Poly2<T>& outline() const { return m_outline; }
+   std::size_t size() const { return m_outline.size(); }
+   bool contains(const Point2<T>& pt) const;
 
  private:
    Point2<T> m_seed;
@@ -40,6 +40,13 @@ template <typename T>
 VoronoiTile<T>::VoronoiTile(Point2<T> seed, Poly2<T> outline)
 : m_seed{std::move(seed)}, m_outline{std::move(outline)}
 {
+}
+
+
+template <typename T>
+bool VoronoiTile<T>::contains(const Point2<T>& pt) const
+{
+   return m_outline.contains(pt) != m_outline.end();
 }
 
 } // namespace geom
